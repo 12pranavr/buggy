c19f28559b49bc791e10c3819f580c5420dcb061
SKULL <12pranav04@gmail.com>
2026-02-19 17:11:56 +0530

    [AI-AGENT] Fix LOGIC in calculator.py line 17

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def subtract(a, b):
     return a - b  # bug: should be a - b
 
 def multiply(a, b):
-    return a + b  # bug: should be a * b
+    return a * b  # bug: should be a * b
 
 def test_add():
     assert add(2, 3) == 5
